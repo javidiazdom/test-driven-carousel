cfbe8497ee2f4d9f7a7ad24196bb080ec2748ef9
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-10-07 13:48:55 +0100

    :x: Children are not rendered yet

--- src/test/CarouselSlide.test.js
+++ src/test/CarouselSlide.test.js
@@ -3,8 +3,15 @@ import { shallow } from 'enzyme';
 import CarouselSlide from '../CarouselSlide';
 
 describe('CarouselSlide', () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<CarouselSlide />);
+  });
   it('renders a <figure>', () => {
-    const wrapper = shallow(<CarouselSlide />);
     expect(wrapper.type()).toBe('figure');
+  });
+  it('renders an <img> and a <figcaption> as children', () => {
+    expect(wrapper.childAt(0).type()).toBe('img');
+    expect(wrapper.childAt(1).type()).toBe('figcaption');
   });
 });
