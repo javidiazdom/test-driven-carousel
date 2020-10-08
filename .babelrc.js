c1b0e4efd975decdde0decbd840698b06cae6dc9
Javier Díaz Domínguez <javidiazdom@gmail.com>
2020-10-08 12:46:49 +0100

    :white_check_mark: Add style tests for the <img>

--- .babelrc.js
+++ .babelrc.js
@@ -2,6 +2,6 @@ module.exports = {
   presets: ['@babel/preset-react', '@babel/preset-env'],
   plugins: [
     '@babel/plugin-proposal-class-properties',
-    'babel-plugin-styled-components',
+    //'babel-plugin-styled-components',
   ],
 };
